35e3090da88881e06540ca049859b039b0dfa6c2
Juan Manuel Bermudez <juanma72152@gmail.com>
2024-11-04 23:41:14 -0500

    Se corrigio el modelo para su correcto funcionamiento, se termino la parte grafica de la pagina y de la parte de prediccion se agregaron los comentarios correspondientes para cada caso

--- app/app.py
+++ app/app.py
@@ -1,12 +1,17 @@
+from sklearn.preprocessing import StandardScaler
 from flask import Flask, render_template, redirect, url_for, request, jsonify
 import joblib
 import os
+import numpy as np
 
 app = Flask(__name__)
 
 # Cargar el modelo
 modelo_path = os.path.join(os.path.dirname(__file__), 'modelo', 'modelo_knn.pkl')
 modelo = joblib.load(modelo_path)
+# Cargar el scaler
+scaler_path = os.path.join(os.path.dirname(__file__), 'modelo', 'scaler.pkl')
+scaler = joblib.load(scaler_path)
 
 # Ruta para la página principal
 @app.route('/')
@@ -21,28 +26,43 @@ def predicciones():
 # API para hacer la predicción
 @app.route('/api/prediccion', methods=['POST'])
 def api_prediccion():
-    data = request.json  # Obtener datos JSON del formulario
+    data = request.json # Obtener datos JSON del formulario
     try:
+        # Validar datos necesarios esten en Json
+        required_fields = ['edad', 'sexo', 'tipo_dolor', 'presion_arterial',
+                           'colesterol', 'azucar_sangre', 'resultados_ecg',
+                           'frecuencia_cardiaca_maxima', 'angina_inducida',
+                           'depresion_st', 'pendiente_st_ejercicio',
+                           'vasos_principales_fluroscopia', 'talasemia']
+        
+        for field in required_fields:
+            if field not in data:
+                return jsonify({'error': f'Missing field: {field}'}), 400
+
         # Extraer y convertir los datos al formato que espera el modelo
         input_features = [
-            float(data['edad']),
+            int(data['edad']),
             1 if data['sexo'].lower() == 'masculino' else 0,
-            float(data['tipo_dolor']),
-            float(data['presion_arterial']),
-            float(data['colesterol']),
-            float(data['azucar_sangre']),
-            float(data['resultados_ecg']),
-            float(data['frecuencia_cardiaca_maxima']),
+            int(data['tipo_dolor']),
+            int(data['presion_arterial']),
+            int(data['colesterol']),
+            int(data['azucar_sangre']),
+            int(data['resultados_ecg']),
+            int(data['frecuencia_cardiaca_maxima']),
             1 if data['angina_inducida'].lower() == 'sí' else 0,
             float(data['depresion_st']),
-            float(data['pendiente_st_ejercicio']),
+            int(data['pendiente_st_ejercicio']),
             int(data['vasos_principales_fluroscopia']),
-            1 if data['talasemia'].lower() == 'sí' else 0
+            int(data['talasemia'])
         ]
-        
+
+        # Convertir a array de numpy y estandarizar los datos de entrada usando el scaler ya entrenado
+        input_features = np.array(input_features).reshape(1, -1)
+        input_features = scaler.transform(input_features)
+
         # Realizar la predicción
-        prediccion = modelo.predict([input_features])[0]  # Obtener el primer elemento de la predicción
-        
+        prediccion = modelo.predict(input_features)[0]  # Obtener el primer elemento de la predicción
+
         # Listas de consejos
         consejos_con_problemas = [
             "1. Mantén una dieta baja en grasas saturadas y azúcares.",
@@ -51,7 +71,7 @@ def api_prediccion():
             "4. Evita el consumo de tabaco y alcohol.",
             "5. Consulta con un médico para un plan de tratamiento adecuado."
         ]
-        
+
         consejos_sin_problemas = [
             "1. Mantén una dieta equilibrada rica en frutas y verduras.",
             "2. Haz ejercicio regularmente para mantener tu corazón sano.",
@@ -59,7 +79,7 @@ def api_prediccion():
             "4. Realiza chequeos médicos de rutina.",
             "5. Mantén una buena salud mental y maneja el estrés."
         ]
-        
+
         # Interpretar el resultado y proporcionar consejos
         if prediccion == 1:
             resultado = "El paciente presenta problemas cardíacos."
@@ -74,8 +94,9 @@ def api_prediccion():
         # Devolver el resultado e incluir consejos en formato JSON
         return jsonify({'resultado': resultado, 'consejos': consejos})
 
+    except ValueError as ve:
+        return jsonify({'error': f'Value error: {str(ve)}'}), 400
     except Exception as e:
-        # En caso de error, devolver el mensaje de error
         return jsonify({'error': str(e)}), 400
 
 # Manejo de errores para página no encontrada
